37ff975865233d9b719471e821cbbee75f5f1c1d
flibertigibet <vadhirbhupathi@gmail.com>
2016-06-14 23:12:33 +0530

    Base model

---
+++ src/components/common/header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Link} from 'react-router';
+const Header = () => {
+  return(
+    <div>
+      <nav>
+          <Link to='/'>Home</Link>
+          { ' | ' }
+          <Link to='about'>About</Link>
+      </nav>
+      <h2 className='jumbotron' >React relay graphql app</h2>
+    </div>
+  );
+}
+
+export default Header;
